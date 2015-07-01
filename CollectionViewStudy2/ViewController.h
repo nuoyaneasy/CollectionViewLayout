b06ddf4297a0e6c656655c0312116a0377fb2192
Chao Yang <doublemeltin@gmail.com>
2015-07-01 23:35:28 +0800

    Initial Commit

---
+++ CollectionViewStudy2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewStudy2
+//
+//  Created by Yang Chao on 7/1/15.
+//  Copyright (c) 2015 Self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
